59dba2435fab52a35805b8ab2224d353d3442ab4
DiegoDAW94 <daw13.2024@gmail.com>
2025-05-21 09:41:58 +0200

    despliegue config

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -2,9 +2,13 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // Cambia esto al dominio de tu frontend
-    'allowed_headers' => ['*'],
+    'allowed_origins' => ['http://localhost:5173', 'http://40.66.41.65:5173', '*'], // Cambia esto al dominio de tu >    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
+
+
+
+
+
